c22f38d1b51b10478c56ff6f5d682635c864efa0
niko.xu <xujun4610@outlook.com>
2017-10-07 10:17:56 +0800

    material done!

--- BizSys.OmniChannelToSAP.Service/Document/MasterDataManagement/Material.cs
+++ BizSys.OmniChannelToSAP.Service/Document/MasterDataManagement/Material.cs
@@ -13,76 +13,47 @@ namespace BizSys.OmniChannelToSAP.Service.Document.MasterDataManagement
     {
         public static Result CreateMaterial(ResultObjects material)
         {
-            /*
+            
             Result result = new Result();
 
-            SAPbobsCOM.BusinessPartners myMaterial = SAP.SAPCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oItems);
+            SAPbobsCOM.Items myMaterial = SAP.SAPCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oItems);
             bool IsExists = myMaterial.GetByKey(material.ItemCode);
 
-            myMaterial.CardType = SAPbobsCOM.BoCardTypes.cCustomer;
-            myMaterial.CardCode = material.ItemCode;
-            myMaterial.CardName = material.ItemDescription;
-            myMaterial.GroupCode = Convert.ToInt32(material.GroupCode);
-            myMaterial.CompanyPrivate = BoCardCompanyTypes.cCompany;
+            myMaterial.ItemType = SAPbobsCOM.ItemTypeEnum.itItems;
+            myMaterial.ItemCode = material.ItemCode;
+            myMaterial.ItemName = material.ItemDescription;
+            myMaterial.ForeignName = material.ForeignDescription;
+            myMaterial.ItemsGroupCode = Convert.ToInt32(material.CategoryCode);
 
+            myMaterial.PurchaseItemsPerUnit = material.NoOfItemsPerPurchaseUnit;
+            myMaterial.PurchaseUnit = material.PurchasingUoM;
+            myMaterial.SalesUnit = material.SalesUoM;
+            myMaterial.SalesItemsPerUnit = material.NoOfItemsPerSalesUnit;
+            myMaterial.SalesVATGroup = BOneCommon.GetTaxByRate(material.SaleTax, "O");
+            myMaterial.PurchaseVATGroup = BOneCommon.GetTaxByRate(material.PurchaseTax, "I");
+            myMaterial.DefaultWarehouse = material.DefaultWarehouse;
+            //首选供应商
+            SAPbobsCOM.Items_PreferredVendors pv = myMaterial.PreferredVendors;
+            for (int i = 0; i < pv.Count; i++)
+            {
+                pv.SetCurrentLine(i);
+                if (!material.PreferredVendor.Equals(pv.BPCode)){
+                    pv.BPCode = material.PreferredVendor;
+                    pv.Add();
+                }
+            }
 
-            myMaterial.ZipCode = material.BillToZipCode;
-            myMaterial.EmailAddress = material.Email;
-            myMaterial.CreditLimit = material.PaidToCredit;
-            myMaterial.SalesPersonCode = -1; //销售员修改；
-            myMaterial.Territory = -2; //区域修改，默认值
-
-            myMaterial.PayTermsGrpCode = -1; //付款条件修改
-            myMaterial.PriceListNum = 1; //默认价格清单修改
-
-            myMaterial.Valid = material.Activation == "Yes" ? BoYesNoEnum.tYES : BoYesNoEnum.tNO;
+            myMaterial.Valid = material.Active == "Yes" ? SAPbobsCOM.BoYesNoEnum.tYES : SAPbobsCOM.BoYesNoEnum.tNO;
             myMaterial.ValidFrom = Convert.ToDateTime(material.ActiveFrom);
             myMaterial.ValidTo = Convert.ToDateTime(material.ActiveTo);
-            myMaterial.Frozen = material.Inactive == "No" ? BoYesNoEnum.tNO : BoYesNoEnum.tYES;
-            myMaterial.FrozenFrom = Convert.ToDateTime(material.InactiveFrom);
-            myMaterial.FrozenTo = Convert.ToDateTime(material.InactiveTo);
-            myMaterial.Cellular = material.MobilePhone;
-            myMaterial.Phone1 = material.Telephone1;
-            myMaterial.Phone2 = material.Telephone2;
-            myMaterial.Currency = material.BPCurrency;
-            myMaterial.FreeText = material.Remarks;
+            //myMaterial.Frozen = material.Inactive == "No" ? BoYesNoEnum.tNO : BoYesNoEnum.tYES;
+            //myMaterial.FrozenFrom = Convert.ToDateTime(material.InactiveFrom);
+            //myMaterial.FrozenTo = Convert.ToDateTime(material.InactiveTo);
 
             //if(!string.IsNullOrEmpty(material.TaxNumber))
             //myMaterial.GTSRegNo = material.TaxNumber;
             //myMaterial.GTSBillingAddrTel =  material.BillingAddress + '-' + material.BillingTelephone;
             //myMaterial.GTSBankAccountNo =  material.HouseBank + '-' + material.Account;
-            foreach (var item in material.CustomerItems)
-            {
-                string addressName = item.City + item.County + item.BillToStreet;
-                if (!string.IsNullOrEmpty(addressName))
-                {
-                    myMaterial.Addresses.AddressName = addressName;
-                    myMaterial.Addresses.Delete();
-                    myMaterial.Addresses.AddressName = addressName;
-                    myMaterial.Addresses.Block = item.BillToStreet;
-                    myMaterial.Addresses.City = item.City;
-                    myMaterial.Addresses.County = item.County;
-                    myMaterial.Addresses.Street = item.BillToStreet;
-                    myMaterial.Addresses.ZipCode = item.BillToZipCode;
-                    myMaterial.Addresses.State = B1Common.BOneCommon.GetAddressCode(item.Province);
-
-                    myMaterial.Addresses.AddressType = BoAddressType.bo_ShipTo;
-                    myMaterial.Addresses.Add();
-                }
-            }
-            myMaterial.ContactPerson = material.ContactPerson;
-            myMaterial.ContactEmployees.SetCurrentLine(0);
-            myMaterial.ContactEmployees.Name = material.ContactPerson;
-            myMaterial.ContactEmployees.Address = material.BillToStreet;
-            myMaterial.ContactEmployees.Fax = material.FaxNumber;
-            myMaterial.ContactEmployees.MobilePhone = material.MobilePhone;
-            myMaterial.ContactEmployees.Phone1 = material.Telephone1;
-            myMaterial.ContactEmployees.Phone2 = material.Telephone2;
-            myMaterial.ContactEmployees.E_Mail = material.Email;
-            myMaterial.ContactEmployees.CityOfBirth = material.City;
-
-            myMaterial.ContactEmployees.Add();
-
 
             int RntCode = 0;
             if (IsExists)
@@ -96,16 +67,17 @@ namespace BizSys.OmniChannelToSAP.Service.Document.MasterDataManagement
             if (RntCode != 0)
             {
                 result.ResultValue = ResultType.False;
-                result.ResultMessage = string.Format("【{0}】客户处理失败，ErrorCode:[{1}],ErrrMsg:[{2}];", material.CustomerCode, SAP.SAPCompany.GetLastErrorCode(), SAP.SAPCompany.GetLastErrorDescription());
+                result.ResultMessage = string.Format("【{0}】物料处理失败，ErrorCode:[{1}],ErrrMsg:[{2}];", material.ItemCode, SAP.SAPCompany.GetLastErrorCode(), SAP.SAPCompany.GetLastErrorDescription());
             }
             else
             {
                 result.ResultValue = ResultType.True;
-                result.ResultMessage = "【" + material.CustomerCode + "】客户处理成功，系统数据：" + SAP.SAPCompany.GetNewObjectKey();
+                result.DocEntry = SAP.SAPCompany.GetNewObjectKey();
+                result.ResultMessage = "【" + material.ItemCode + "】物料处理成功，系统数据：" + result.DocEntry;
             }
             System.Runtime.InteropServices.Marshal.FinalReleaseComObject(myMaterial);
             return result;
-            */
+
         }
     }
 }
